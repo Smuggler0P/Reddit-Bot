1674dda54fc7369ce108242a27bed7cd4aa3fabc
Smuggler0P <90851567+Smuggler0P@users.noreply.github.com>
2023-07-30 12:48:02 +0530

    updated bot

---
+++ bot_read.py
@@ -0,0 +1,10 @@
+import praw
+reddit= praw.Reddit('bot1')
+subreddit=reddit.subreddit("learnpython")
+
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
+    
